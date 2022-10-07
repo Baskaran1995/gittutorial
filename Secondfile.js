d51092cb53bd5a7cf0017c45069090773074caa1
Baskaran1995 <114877287+Baskaran1995@users.noreply.github.com>
2022-10-07 06:42:40 +0530

    Update Secondfile.js

---
+++ Secondfile.js
@@ -0,0 +1,41 @@
+console.dir(document);
+
+console.log(document.domain);
+
+console.log.(document.URL);
+
+console.log(document.title);
+
+console.log(document.doctype);
+
+console.log(document.head);
+
+console.log(document.body);
+
+console.log(document.all);
+
+console.log(document.all[all]);
+
+console.log(documents.forms[0]);
+
+console.log(documents.links);
+
+console.log(documents.images);
+
+//GETELEMENTBYID//
+
+console.log(document.getElementId('header-title'));
+
+var headerTitle = document.getElementId('header-title'));
+
+console.log(headerTitle);
+
+headerTitle.textContent = 'Hello';
+
+headerTitle.innerText = 'Goodbye';
+
+//console.log(headerTitle.innerText);
+
+headerTitle.innerHTML = '<h3>Hello</h3>';
+
+headerTitle.style.border-Bottom = 'solid 3px #000';
